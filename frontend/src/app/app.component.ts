39631004200539dba9020e24598f744ebbd4b1a4
Emito Khoza <emito.khoza@gmail.com>
2024-05-22 22:36:40 +0200

    Added user persistence

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './shared/services/auth.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,5 +9,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.authService.user$.subscribe((user) => {
+      if (user) {
+        console.log('User logged in: ', user);
+        this.authService.currentUserSig.set({
+          uid: user.uid,
+          email: user.email!,
+          username: user.displayName!
+        });
+      } else {
+        this.authService.currentUserSig.set(null);
+      }
+
+      console.log(this.authService.currentUserSig());
+    });
+  }
 }
--- frontend/src/app/shared/services/auth.service.ts
+++ frontend/src/app/shared/services/auth.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Injectable, signal } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, user } from '@angular/fire/auth';
 import { Observable, from } from 'rxjs';
 import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, setDoc } from '@angular/fire/firestore';
+import { UserInterface } from '../models/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  user$ = user(this.firebaseAuth);
+  currentUserSig = signal<UserInterface | null | undefined>(undefined);
 
   constructor(private firebaseAuth: Auth, private firestore: Firestore) { }
 
