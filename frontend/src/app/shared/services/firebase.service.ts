31d5137310a1d9ebcbfaf90c15952e4942d6e65c
Emito Khoza <emito.khoza@gmail.com>
2024-05-22 17:51:55 +0200

    Updated and added firebase service

---
+++ frontend/src/app/shared/services/firebase.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+  firestore = inject(Firestore);
+  wikiSpaces = collection(this.firestore, 'wikiSpaces');
+
+  constructor() { }
+
+  getWikiSpaces(): Observable<any> {
+    return collectionData(this.wikiSpaces, { idField: 'id' }) as Observable<any>;
+  }
+}
