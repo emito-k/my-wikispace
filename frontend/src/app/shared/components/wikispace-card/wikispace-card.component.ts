2fd7d8b149d22d3a4657319bbaf0ce7e7fd15ca0
Emito Khoza <emito.khoza@gmail.com>
2024-05-23 01:51:53 +0200

    Updated the wikispace card

--- frontend/src/app/shared/components/wikispace-card/wikispace-card.component.ts
+++ frontend/src/app/shared/components/wikispace-card/wikispace-card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import { WikispaceInterface } from '../../models/wikispace.interface';
 @Component({
   selector: 'app-wikispace-card',
   standalone: true,
-  imports: [],
+  imports: [
+    MatProgressBarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCardModule
+  ],
   templateUrl: './wikispace-card.component.html',
   styleUrl: './wikispace-card.component.css'
 })
 export class WikispaceCardComponent {
-
+  @Input({ required: true }) wikispace!: WikispaceInterface;
 }
