ef74e26c40d2de5de2d73c016f2736502385cdad
Emito Khoza <emito.khoza@gmail.com>
2024-05-22 17:45:07 +0200

    Added frontend interface

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+import { routes } from './app.routes';
+
+const firebaseConfig = environment.firebaseConfig;
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
+
+};
