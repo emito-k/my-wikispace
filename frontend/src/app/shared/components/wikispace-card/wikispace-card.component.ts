c3ad449af5ac26b719e2216bd177292153f54b8c
Emito Khoza <emito.khoza@gmail.com>
2024-05-23 01:40:52 +0200

    Created component

---
+++ frontend/src/app/shared/components/wikispace-card/wikispace-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wikispace-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './wikispace-card.component.html',
+  styleUrl: './wikispace-card.component.css'
+})
+export class WikispaceCardComponent {
+
+}
