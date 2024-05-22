8a9c330f2da93edb3584207dc81df6ee51446804
Emito Khoza <emito.khoza@gmail.com>
2024-05-22 21:56:32 +0200

    Updated login page

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-  constructor() {}
+  form = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    // this.http.post('/api/login', this.form.value).subscribe(() => {
+    //   this.router.navigate(['/']);
+    // });.
+  }
 }
