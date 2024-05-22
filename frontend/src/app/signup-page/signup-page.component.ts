f96a9d0e75a5d1a526c2890e4188e5fb82ae974e
Emito Khoza <emito.khoza@gmail.com>
2024-05-22 22:00:59 +0200

    Updated the signup page

--- frontend/src/app/signup-page/signup-page.component.ts
+++ frontend/src/app/signup-page/signup-page.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule} from '@angular/forms';
+import axios from 'axios';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
 })
 export class SignupPageComponent {
+  form = this.fb.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  onSubmit() {
+    axios.post('/api/signup', this.form.value).then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
 }
