d0fe870921b8ac0fb4a93c0cb61879bd986a90d8
Emito Khoza <emito.khoza@gmail.com>
2024-05-23 17:34:50 +0200

    Added wiki pages feed page

---
+++ frontend/src/app/shared/components/wiki-pages-feed/wiki-pages-feed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wiki-pages-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './wiki-pages-feed.component.html',
+  styleUrl: './wiki-pages-feed.component.css'
+})
+export class WikiPagesFeedComponent {
+
+}
