585cb881c705a3f50e4d78e190c4adcfee594d54
Emito Khoza <emito.khoza@gmail.com>
2024-05-23 23:59:02 +0200

    Added wiki edit page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { WikispacePageComponent } from './wikispace-page/wikispace-page.component';
+import { WikiEditPageComponent } from './wiki-edit-page/wiki-edit-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
-  { path: 'wiki/:wiki_id', component: WikispacePageComponent }
+  { path: 'wiki/:wiki_id', component: WikispacePageComponent },
+  { path: 'wiki/:wiki_id/w/:wiki_page_id', component: WikiEditPageComponent }
 ];
---
+++ frontend/src/app/wiki-edit-page/wiki-edit-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wiki-edit-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './wiki-edit-page.component.html',
+  styleUrl: './wiki-edit-page.component.css'
+})
+export class WikiEditPageComponent {
+
+}
