bed5917347fce7b683ae1f9d93756801915b50b3
Emito Khoza <emito.khoza@gmail.com>
2024-05-23 10:55:18 +0200

    Updated the dialog form for creaitng

--- frontend/src/app/shared/components/dialog-create-wikispace-form/dialog-create-wikispace-form.component.ts
+++ frontend/src/app/shared/components/dialog-create-wikispace-form/dialog-create-wikispace-form.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { PermissionInterface } from '../../models/permission.interface';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dialog-create-wikispace-form',
   standalone: true,
   imports: [
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './dialog-create-wikispace-form.component.html',
   styleUrl: './dialog-create-wikispace-form.component.css'
 })
 export class DialogCreateWikispaceFormComponent {
+  permissions: PermissionInterface[] = [
+    { name: 'Public', description: 'Everyone can view and edit' },
+    { name: 'Protected', description: 'Everyone can view pages, only wiki members users can edit' },
+    { name: 'Private', description: 'Only wiki members can view and edit' }
+  ];
 
+  chosenPermission: PermissionInterface = this.permissions[0];
+
+  description: string = '';
+  constructor(public dialog: MatDialog) { }
 }
